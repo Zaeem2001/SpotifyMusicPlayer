a7bf0444cdad26cc6a04b9bb83c0af16b278eacc
Zaeem Ghauri <86279858+Zaeem2001@users.noreply.github.com>
2021-07-02 03:16:30 -0400

    Added up and down buttons for volume input

--- Spotify.ino
+++ Spotify.ino
@@ -7,9 +7,11 @@ String screen_info;
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(2);
+  pinMode(6, INPUT); // set pin 6 to be input to push button down
   pinMode(5, INPUT); // set pin 5 to be input to push button right
   pinMode(4, INPUT); // set pin 4 to be input to push button select
   pinMode(3, INPUT); // set pin 3 to be input to push button left
+  pinMode(2, INPUT); // set pin 2 to be input to push button up
   
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
@@ -21,17 +23,28 @@ void selectButton (){
   delay(250);
 }
 
-void rightButton (){
+void rightButton(){
   Serial.print("RIGHT\n");
   delay(250);
 }
 
-void leftButton (){
+void leftButton(){
   Serial.print("LEFT\n");
   delay(250);
 }
 
+void upButton(){
+  Serial.print("UP\n");
+  delay(250);
+}
+
+void downButton(){
+  Serial.print("DOWN\n");
+  delay(250);
+}
+
 void loop() {
+  
   // button detection 
   if (digitalRead(5) > 0)
     rightButton();
@@ -41,6 +54,12 @@ void loop() {
 
   else if (digitalRead(3) > 0)
     leftButton();
+
+  else if (digitalRead(2) > 0)
+    upButton();
+
+  else if (digitalRead(6) > 0)
+    downButton();
   
   // serial communication with python
   if (Serial.available() > 0)
